67688a997a08e5bdedb94fa9900d83cbe6f46d19
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-01-26 12:56:31 -0800

    Add inverse triangle function

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ function drawTriangle(n) {
     }
   }
 
-//   function drawInverseTriangle(n) {
-//     // first, loop through the number of rows
-//     for(i = n; i <= 0; i--){
-//         row = "";
-//         for(j = i; j < 0; j--) {
-//             row = row.concat("", "*");
-//         }
-//         console.log(row);
-//     }
-//   }
+  function drawInverseTriangle(n) {
+    // first, loop through the number of rows
+    for(i = n; i >= 0; i--){
+        row = "";
+        for(j = i; j > 0; j--) {
+            row = row.concat("", "*");
+        }
+        console.log(row);
+    }
+  }
 
-  drawTriangle(5);
-  //drawInverseTriangle(5);
+  drawTriangle(10);
+  console.log("---------------------------")
+  drawInverseTriangle(10);
